c4acefb0cd597ddd6c7a39136b2e34688a6af878
Shantanu Sutar <shantanusutar17@gmail.com>
2023-03-11 15:30:11 +0530

    Tabs Display First Job

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,11 +6,11 @@ const url = "https://course-api.com/react-tabs-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
-  const [value, setValue] = useState([0]);
+  const [value, setValue] = useState(0);
 
   const fetchJobs = async () => {
     const response = await fetch(url);
-    const newJobs = response.json();
+    const newJobs = await response.json();
     setJobs(newJobs);
     setLoading(false);
   };
@@ -26,8 +26,32 @@ function App() {
       </section>
     );
   }
-
-  return <h2>jobs</h2>;
+  const { company, dates, duties, title } = jobs[value];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* btn container */}
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App;
